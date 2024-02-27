d8091c68689c417b071dc4da8f0b15a513be0d54
Исупов Андрей Николаевич <isupov.a@niisvt.ru>
2024-02-27 09:43:45 +0300

    set linked properties from config

--- modules/openhasp/hasp_panels_edit.inc.php
+++ modules/openhasp/hasp_panels_edit.inc.php
@@ -2,14 +2,14 @@
 /*
 * @version 0.1 (wizard)
 */
-  if ($this->owner->name=='panel') {
-   $out['CONTROLPANEL']=1;
-  }
-  $table_name='hasp_panels';
-  $rec=SQLSelectOne("SELECT * FROM $table_name WHERE ID='$id'");
-  if ($this->mode=='update') {
-   $ok=1;
-  
+if ($this->owner->name=='panel') {
+    $out['CONTROLPANEL']=1;
+}
+$table_name='hasp_panels';
+$rec=SQLSelectOne("SELECT * FROM $table_name WHERE ID='$id'");
+if ($this->mode=='update') {
+    $ok=1;
+
 
     if ($this->tab == '') {
         $rec['TITLE'] = gr('title');
@@ -26,6 +26,7 @@
     }
 
     if ($this->tab == 'config') {
+        $old_config = $rec['PANEL_CONFIG'];
         $rec['PANEL_CONFIG'] = gr('panel_config');
         if (!is_array(json_decode($rec['PANEL_CONFIG'], true))) {
             $ok = 0;
@@ -39,24 +40,26 @@
         */
     }
     
-  //UPDATING RECORD
-   if ($ok) {
-    if ($rec['ID']) {
-     SQLUpdate($table_name, $rec); // update
+    //UPDATING RECORD
+    if ($ok) {
+        if ($rec['ID']) {
+            SQLUpdate($table_name, $rec); // update
+        } else {
+            $new_rec=1;
+            $rec['ID']=SQLInsert($table_name, $rec); // adding new record
+        }
+        $out['OK']=1;
+        $this->unsetLinked($old_config);
+        $this->setLinked($rec['PANEL_CONFIG']);
     } else {
-     $new_rec=1;
-     $rec['ID']=SQLInsert($table_name, $rec); // adding new record
+        $out['ERR']=1;
     }
-    $out['OK']=1;
-   } else {
-    $out['ERR']=1;
-   }
-  }
-  if (is_array($rec)) {
-   foreach($rec as $k=>$v) {
-    if (!is_array($v)) {
-     $rec[$k]=htmlspecialchars($v);
+}
+if (is_array($rec)) {
+    foreach($rec as $k=>$v) {
+        if (!is_array($v)) {
+            $rec[$k]=htmlspecialchars($v);
+        }
     }
-   }
-  }
-  outHash($rec, $out);
+}
+outHash($rec, $out);
--- modules/openhasp/openhasp.class.php
+++ modules/openhasp/openhasp.class.php
@@ -9,191 +9,211 @@
 //
 //
 class openhasp extends module {
-/**
+    /**
 * openhasp
 *
 * Module class constructor
 *
 * @access private
 */
-function __construct() {
-  $this->name="openhasp";
-  $this->title="OpenHasp";
-  $this->module_category="<#LANG_SECTION_DEVICES#>";
-  $this->checkInstalled();
-}
-/**
+    function __construct() {
+        $this->name="openhasp";
+        $this->title="OpenHasp";
+        $this->module_category="<#LANG_SECTION_DEVICES#>";
+        $this->checkInstalled();
+    }
+    /**
 * saveParams
 *
 * Saving module parameters
 *
 * @access public
 */
-function saveParams($data=1) {
- $p=array();
- if (IsSet($this->id)) {
-  $p["id"]=$this->id;
- }
- if (IsSet($this->view_mode)) {
-  $p["view_mode"]=$this->view_mode;
- }
- if (IsSet($this->edit_mode)) {
-  $p["edit_mode"]=$this->edit_mode;
- }
- if (IsSet($this->tab)) {
-  $p["tab"]=$this->tab;
- }
- return parent::saveParams($p);
-}
-/**
+    function saveParams($data=1) {
+        $p=array();
+        if (IsSet($this->id)) {
+            $p["id"]=$this->id;
+        }
+        if (IsSet($this->view_mode)) {
+            $p["view_mode"]=$this->view_mode;
+        }
+        if (IsSet($this->edit_mode)) {
+            $p["edit_mode"]=$this->edit_mode;
+        }
+        if (IsSet($this->tab)) {
+            $p["tab"]=$this->tab;
+        }
+        return parent::saveParams($p);
+    }
+    /**
 * getParams
 *
 * Getting module parameters from query string
 *
 * @access public
 */
-function getParams() {
-  global $id;
-  global $mode;
-  global $view_mode;
-  global $edit_mode;
-  global $tab;
-  if (isset($id)) {
-   $this->id=$id;
-  }
-  if (isset($mode)) {
-   $this->mode=$mode;
-  }
-  if (isset($view_mode)) {
-   $this->view_mode=$view_mode;
-  }
-  if (isset($edit_mode)) {
-   $this->edit_mode=$edit_mode;
-  }
-  if (isset($tab)) {
-   $this->tab=$tab;
-  }
-}
-/**
+    function getParams() {
+        global $id;
+        global $mode;
+        global $view_mode;
+        global $edit_mode;
+        global $tab;
+        if (isset($id)) {
+            $this->id=$id;
+        }
+        if (isset($mode)) {
+            $this->mode=$mode;
+        }
+        if (isset($view_mode)) {
+            $this->view_mode=$view_mode;
+        }
+        if (isset($edit_mode)) {
+            $this->edit_mode=$edit_mode;
+        }
+        if (isset($tab)) {
+            $this->tab=$tab;
+        }
+    }
+    /**
 * Run
 *
 * Description
 *
 * @access public
 */
-function run() {
- global $session;
-  $out=array();
-  if ($this->action=='admin') {
-   $this->admin($out);
-  } else {
-   $this->usual($out);
-  }
-  if (IsSet($this->owner->action)) {
-   $out['PARENT_ACTION']=$this->owner->action;
-  }
-  if (IsSet($this->owner->name)) {
-   $out['PARENT_NAME']=$this->owner->name;
-  }
-  $out['VIEW_MODE']=$this->view_mode;
-  $out['EDIT_MODE']=$this->edit_mode;
-  $out['MODE']=$this->mode;
-  $out['ACTION']=$this->action;
-  $out['TAB']=$this->tab;
-  $this->data=$out;
-  $p=new parser(DIR_TEMPLATES.$this->name."/".$this->name.".html", $this->data, $this);
-  $this->result=$p->result;
-}
-/**
+    function run() {
+        global $session;
+        $out=array();
+        if ($this->action=='admin') {
+            $this->admin($out);
+        } else {
+            $this->usual($out);
+        }
+        if (IsSet($this->owner->action)) {
+            $out['PARENT_ACTION']=$this->owner->action;
+        }
+        if (IsSet($this->owner->name)) {
+            $out['PARENT_NAME']=$this->owner->name;
+        }
+        $out['VIEW_MODE']=$this->view_mode;
+        $out['EDIT_MODE']=$this->edit_mode;
+        $out['MODE']=$this->mode;
+        $out['ACTION']=$this->action;
+        $out['TAB']=$this->tab;
+        $this->data=$out;
+        $p=new parser(DIR_TEMPLATES.$this->name."/".$this->name.".html", $this->data, $this);
+        $this->result=$p->result;
+    }
+    /**
 * BackEnd
 *
 * Module backend
 *
 * @access public
 */
-function admin(&$out) {
-    $this->getConfig();
+    function admin(&$out) {
+        $this->getConfig();
 
-    $out['MQTT_HOST'] = $this->config['MQTT_HOST'];
-    $out['MQTT_PORT'] = $this->config['MQTT_PORT'];
-    $out['MQTT_QUERY'] = $this->config['MQTT_QUERY'];
-    if (!$out['MQTT_HOST']) {
-        $out['MQTT_HOST'] = 'localhost';
-    }
-    if (!$out['MQTT_PORT']) {
-        $out['MQTT_PORT'] = '1883';
-    }
-    $out['MQTT_USERNAME'] = $this->config['MQTT_USERNAME'];
-    $out['MQTT_PASSWORD'] = $this->config['MQTT_PASSWORD'];
-    $out['MQTT_AUTH'] = $this->config['MQTT_AUTH'];
-    $out['DEBUG_MODE'] = $this->config['DEBUG_MODE'];
+        $out['MQTT_HOST'] = $this->config['MQTT_HOST'];
+        $out['MQTT_PORT'] = $this->config['MQTT_PORT'];
+        $out['MQTT_QUERY'] = $this->config['MQTT_QUERY'];
+        if (!$out['MQTT_HOST']) {
+            $out['MQTT_HOST'] = 'localhost';
+        }
+        if (!$out['MQTT_PORT']) {
+            $out['MQTT_PORT'] = '1883';
+        }
+        $out['MQTT_USERNAME'] = $this->config['MQTT_USERNAME'];
+        $out['MQTT_PASSWORD'] = $this->config['MQTT_PASSWORD'];
+        $out['MQTT_AUTH'] = $this->config['MQTT_AUTH'];
+        $out['DEBUG_MODE'] = $this->config['DEBUG_MODE'];
 
-    if ($this->view_mode == 'update_settings') {
-        $this->config['MQTT_HOST'] = gr('mqtt_host', 'trim');
-        $this->config['MQTT_USERNAME'] = gr('mqtt_username', 'trim');
-        $this->config['MQTT_PASSWORD'] = gr('mqtt_password', 'trim');
-        $this->config['MQTT_AUTH'] = gr('mqtt_auth', 'int');
-        $this->config['MQTT_PORT'] = gr('mqtt_port', 'int');
-        $this->config['MQTT_QUERY'] = gr('mqtt_query', 'trim');
-        $this->saveConfig();
-        setGlobal('cycle_openhaspControl', 'restart');
-        $this->redirect("?");
-    }
-        
-    if (isset($this->data_source) && !$_GET['data_source'] && !$_POST['data_source']) {
-        $out['SET_DATASOURCE']=1;
-    }
-    if ($this->data_source=='hasp_panels' || $this->data_source=='') {
-        if ($this->view_mode=='' || $this->view_mode=='search_hasp_panels') {
-            $this->search_hasp_panels($out);
-        }
-        if ($this->view_mode=='edit_hasp_panels') {
-            $this->edit_hasp_panels($out, $this->id);
-        }
-        if ($this->view_mode=='delete_hasp_panels') {
-            $this->delete_hasp_panels($this->id);
+        if ($this->view_mode == 'update_settings') {
+            $this->config['MQTT_HOST'] = gr('mqtt_host', 'trim');
+            $this->config['MQTT_USERNAME'] = gr('mqtt_username', 'trim');
+            $this->config['MQTT_PASSWORD'] = gr('mqtt_password', 'trim');
+            $this->config['MQTT_AUTH'] = gr('mqtt_auth', 'int');
+            $this->config['MQTT_PORT'] = gr('mqtt_port', 'int');
+            $this->config['MQTT_QUERY'] = gr('mqtt_query', 'trim');
+            $this->saveConfig();
+            setGlobal('cycle_openhaspControl', 'restart');
             $this->redirect("?");
         }
+        
+        if (isset($this->data_source) && !$_GET['data_source'] && !$_POST['data_source']) {
+            $out['SET_DATASOURCE']=1;
+        }
+        if ($this->data_source=='hasp_panels' || $this->data_source=='') {
+            if ($this->view_mode=='' || $this->view_mode=='search_hasp_panels') {
+                $this->search_hasp_panels($out);
+            }
+            if ($this->view_mode=='edit_hasp_panels') {
+                $this->edit_hasp_panels($out, $this->id);
+            }
+            if ($this->view_mode=='delete_hasp_panels') {
+                $this->delete_hasp_panels($this->id);
+                $this->redirect("?");
+            }
+        }
     }
-}
-/**
+    /**
 * FrontEnd
 *
 * Module frontend
 *
 * @access public
 */
-function usual(&$out) {
- $this->admin($out);
-}
-/**
+    function usual(&$out) {
+        $this->admin($out);
+    }
+    /**
 * hasp_panels search
 *
 * @access public
 */
- function search_hasp_panels(&$out) {
-  require(dirname(__FILE__).'/hasp_panels_search.inc.php');
- }
-/**
+    function search_hasp_panels(&$out) {
+        require(dirname(__FILE__).'/hasp_panels_search.inc.php');
+    }
+    /**
 * hasp_panels edit/add
 *
 * @access public
 */
- function edit_hasp_panels(&$out, $id) {
-  require(dirname(__FILE__).'/hasp_panels_edit.inc.php');
- }
-/**
+    function edit_hasp_panels(&$out, $id) {
+        require(dirname(__FILE__).'/hasp_panels_edit.inc.php');
+    }
+    /**
 * hasp_panels delete record
 *
 * @access public
 */
- function delete_hasp_panels($id) {
-  $rec=SQLSelectOne("SELECT * FROM hasp_panels WHERE ID='$id'");
-  // some action for related tables
-  SQLExec("DELETE FROM hasp_panels WHERE ID='".$rec['ID']."'");
- }
- 
-     
+    function delete_hasp_panels($id) {
+        $rec=SQLSelectOne("SELECT * FROM hasp_panels WHERE ID='$id'");
+        // some action for related tables
+        SQLExec("DELETE FROM hasp_panels WHERE ID='".$rec['ID']."'");
+    }
+
+    function unsetLinked($config){
+        $pattern = '/%([^%]+)\.([^%]+)%/';
+        preg_match_all($pattern, $config, $matches);
+
+        $values = array();
+        for ($i = 0; $i < count($matches[0]); $i++) {
+            removeLinkedProperty($matches[1][$i], $matches[2][$i], $this->name);
+        }
+    }
+
+    function setLinked($config){
+        $pattern = '/%([^%]+)\.([^%]+)%/';
+        preg_match_all($pattern, $config, $matches);
+
+        $values = array();
+        for ($i = 0; $i < count($matches[0]); $i++) {
+            addLinkedProperty($matches[1][$i], $matches[2][$i], $this->name);
+        }
+    }
+
+    
     function sendMQTTCommand($topic, $command)
     {
         DebMes("Sending custom command to $topic: " . $command, 'openhasp');
@@ -234,15 +254,15 @@ function usual(&$out) {
         $topic = $root_path . '/command/'.$key;
         $this->sendMQTTCommand($topic, $value);
     }
- 
+
     function api($params)
     {
         if ($_REQUEST['topic']) {
             $this->processMessage($_REQUEST['topic'], $_REQUEST['msg']);
         }
     }
- 
-     function processPanelMessage($panel, $topic, $msg)
+
+    function processPanelMessage($panel, $topic, $msg)
     {
         
         DebMes("Processing (" . $panel['TITLE'] . ")  $topic :\n$msg", 'openhasp');
@@ -250,7 +270,7 @@ function usual(&$out) {
         //$config = $this->getPanelConfig($panel['PANEL_CONFIG']);
         
     }
- 
+
     function processMessage($topic, $msg)
     {
         $panels = SQLSelect("SELECT ID, MQTT_PATH FROM hasp_panels");
@@ -262,7 +282,7 @@ function usual(&$out) {
             }
         }
     }
- 
+
     function processSubscription($event, $details='') {
         $this->getConfig();
         if ($event=='SAY') {
@@ -271,54 +291,54 @@ function usual(&$out) {
             //...
         }
     }
- 
+
     function processCycle() {
         $this->getConfig();
         //to-do
     }
-/**
+    /**
 * Install
 *
 * Module installation routine
 *
 * @access private
 */
- function install($data='') {
-  subscribeToEvent($this->name, 'SAY');
-  parent::install();
- }
-/**
+    function install($data='') {
+        subscribeToEvent($this->name, 'SAY');
+        parent::install();
+    }
+    /**
 * Uninstall
 *
 * Module uninstall routine
 *
 * @access public
 */
- function uninstall() {
-  SQLExec('DROP TABLE IF EXISTS hasp_panels');
-  parent::uninstall();
- }
-/**
+    function uninstall() {
+        SQLExec('DROP TABLE IF EXISTS hasp_panels');
+        parent::uninstall();
+    }
+    /**
 * dbInstall
 *
 * Database installation routine
 *
 * @access private
 */
- function dbInstall($data) {
-/*
+    function dbInstall($data) {
+        /*
 hasp_panels - 
 */
-  $data = <<<EOD
- hasp_panels: ID int(10) unsigned NOT NULL auto_increment
- hasp_panels: TITLE varchar(100) NOT NULL DEFAULT ''
- hasp_panels: MQTT_PATH varchar(100) NOT NULL DEFAULT '' 
- hasp_panels: CURRENT_PAGE varchar(100) NOT NULL DEFAULT ''
- hasp_panels: PANEL_CONFIG text NOT NULL DEFAULT ''
-EOD;
-  parent::dbInstall($data);
- }
-// --------------------------------------------------------------------
+        $data = <<<EOD
+        hasp_panels: ID int(10) unsigned NOT NULL auto_increment
+        hasp_panels: TITLE varchar(100) NOT NULL DEFAULT ''
+        hasp_panels: MQTT_PATH varchar(100) NOT NULL DEFAULT '' 
+        hasp_panels: CURRENT_PAGE varchar(100) NOT NULL DEFAULT ''
+        hasp_panels: PANEL_CONFIG text NOT NULL DEFAULT ''
+        EOD;
+        parent::dbInstall($data);
+    }
+    // --------------------------------------------------------------------
 }
 /*
 *
